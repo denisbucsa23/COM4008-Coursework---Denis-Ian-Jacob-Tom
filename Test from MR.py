710765b0125515b8b92191d6d5ada97b4e797473
sexylion69 <22406993@bucks.ac.uk>
2024-12-03 13:59:59 +0000

    changed name

--- Test from MR.py
+++ Test from MR.py
@@ -10,7 +10,7 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Space Invaders")
+pygame.display.set_caption("Alien Attack")
 
 # Colors
 BLACK = (0, 0, 0)
